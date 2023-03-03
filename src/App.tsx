686df0af8756f04669f096379fb39618ff8f8c7e
William Cheung <william.cheung.c@gmail.com>
2023-03-03 16:04:40 +0000

    added lesson toggle using Leva

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Leva, useControls } from "leva";
 import { Perf } from "r3f-perf";
@@ -6,8 +7,9 @@ import "./App.css";
 import Lesson43 from "./lesson-43/Lesson43";
 
 function App() {
-  const { perfVisible } = useControls({
+  const { perfVisible, lesson } = useControls({
     perfVisible: false,
+    lesson: { value: 43, options: [43, 44] },
   });
 
   return (
@@ -17,7 +19,16 @@ function App() {
       <Canvas>
         {perfVisible ? <Perf position="top-left" /> : null}
 
-        <Lesson43 />
+        {(() => {
+          switch (lesson) {
+            case 43:
+              return <Lesson43 />;
+            case 44:
+              return <Text>Hello</Text>;
+            default:
+              return null;
+          }
+        })()}
       </Canvas>
     </>
   );
